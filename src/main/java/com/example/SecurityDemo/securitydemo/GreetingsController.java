4851b23243e92b030a60b62fff6118576c2e1567
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-08 18:00:29 +0300

    update

---
+++ src/main/java/com/example/SecurityDemo/securitydemo/GreetingsController.java
@@ -0,0 +1,13 @@
+package com.example.SecurityDemo.securitydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    
+@GetMapping("/hello")
+public String hello(){
+    return "hello";
+}
+}
